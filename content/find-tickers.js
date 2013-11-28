7de9c867979cf94c40d3bfe320e2c87d55b1748e
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-11-28 13:49:29 -0800

    Refactoring content script, getting popup to communicate with content script.

--- content/find-tickers.js
+++ content/find-tickers.js
@@ -1,28 +1,43 @@
 
-jQuery(document).ready(function($) {
-
+GSTContent = function() {};
+/**
+ * Detect ticker symbols on current page by matching href urls to patterns.
+ *
+ * Static method.
+ *
+ * @param array patterns
+ *   Each element containing regex pattern in the form of /pattern/i.
+ *   Do not enclose the pattern in a sting.
+ *
+ * @return array
+ *   Each element being a symbol found, or an empty array if none found.
+ */
+GSTContent.findTickers = function(patterns) {
     // Detect ticker symbols by matching a href urls to these patterns.
-    var patterns = [
-        /.*(ticker|symb).*?([A-Z]+)/,
-    ];
+    if (typeof(patterns) == 'undefined' || patterns.length < 1) {
+        patterns = [
+            '.*(ticker|symb).*?([A-Z]+)',
+        ];
+    }
 
     // Store ticker symbols found in this array.
     var tickers = [];
-
     // Iterate through all 'a' elements.
     $('a').each(function() {
         var href = $(this).attr('href');
         // If the element has a 'href' attribute.
         if (typeof(href) != 'undefined') {
-        	href = decodeURIComponent(href);
-            var match = href.match(patterns[0]);
-            // If the href attribute matches one of our patterns.
-            if (match) {
-                tickers.push(match[2]);
+            href = decodeURIComponent(href);
+            for (i=0; i<patterns.length; i++) {
+                var match;
+                var regex = new RegExp(patterns[i], 'g');
+                // If the href attribute matches one of our patterns.
+                while ((match = regex.exec(href)) !== null) {
+                    tickers.push(match[2]);
+                }
             }
         }
     });
-
     // Remove any duplicates from tickers array.
     var uniqueTickers = [];
     $.each(tickers, function(i, el){
@@ -30,6 +45,24 @@ jQuery(document).ready(function($) {
     });
     tickers = uniqueTickers;
 
+    return tickers;
+};
+
+// Expose local javascript as an API to other tabs, popup, and background pages.
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method == 'findTickers') {
+        var tickers = GSTContent.findTickers(request.patterns);
+        sendResponse({status: 1, tickers: tickers});
+    } else {
+        sendResponse({status: 0});
+    }
+});
+
+jQuery(document).ready(function($) {
+
+    var tickers = GSTContent.findTickers([
+        '.*(ticker|symb).*?([A-Z]+)',
+    ]);
     // Create our box.
     if (tickers.length) {
         $('body').append('<div id="cstContainer">Test</div>');
--- popup/popup.js
+++ popup/popup.js
@@ -2,5 +2,16 @@
 // Setup initial values when popup is first opened.
 jQuery('document').ready(function($) {
     var background = chrome.extension.getBackgroundPage();
-});
+    var patterns = [
+        '.*(ticker|symb).*?([A-Z]+)',
+    ];
 
+    // Ask current tab if it has any tickers.
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      var msg = {method: "findTickers", patterns: patterns};
+      chrome.tabs.sendMessage(tabs[0].id, msg, function(response) {
+        document.write(response.tickers.join());
+      });
+    });
+
+});
