5a36ffac63859be784a7d69a0a83ffac85196ddf
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-12-09 00:21:58 -0800

    Updating.

--- content/find-symbols.js
+++ content/find-symbols.js
@@ -11,31 +11,32 @@ showBar = function(symbols, tickerbar) {
     var text = '';
     // Loop through each variable to be displayed.
     for (symbol in symbols.tickers) {
-        text = text + symbol + ': ';
+        text = text + '<div class="cst-tickerbar-variable">';
         var ticker = symbols.tickers[symbol];
         // Loop through each item in the tickerbar for this variable.
         for (var i=0; i<tickerbar.items.length; i++) {
             if (tickerbar.items[i].show == true) {
                 var source = tickerbar.items[i].source;
+                var new_source = source;
                 var match;
                 var regex = new RegExp('\\[\\[([a-z_]+)\\]\\]', 'g');
                 // Search for metrics on each item and replace them with the
                 // values cached for this variable.
                 while ((match = regex.exec(source)) !== null) {
                     if (typeof(ticker.resource.cache.metrics[match[1]]) != 'undefined') {
-                        source = source.replace('[['+match[1]+']]', ticker.resource.cache.metrics[match[1]].value);
+                        new_source = new_source.replace('[['+match[1]+']]', ticker.resource.cache.metrics[match[1]].value);
                     }
                 }
-                text = text + source + ' ';
+                // Replace the 'variable' variable.
+                new_source = new_source.replace('[[VARIABLE]]', symbol);
+                text = text + '<div class="cst-tickerbar-item">' + new_source + '</div>';
             }
         }
-        // Replace the 'variable' variable.
-        text = text.replace('[[VARIABLE]]', symbol);
-        text = text + ' &nbsp; &nbsp; ';
+        text = text + '</div>';
     }
     if (text.length) {
-        $('body').append('<div id="cstContainer">Test</div>');
-        $('#cstContainer').html('<p>'+text+'</p>');
+        $('body').append('<div id="cst-tickerbar">Test</div>');
+        $('#cst-tickerbar').html(text);
         $('html').css('position', 'relative');
         $('html').css({'margin-top':'30px'});
     }
