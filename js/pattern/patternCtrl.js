686337d6bbafd86172610a64fa63fdd738a84028
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2014-02-19 13:55:37 -0800

    Removing some bugs in pattern config.

--- js/pattern/patternCtrl.js
+++ js/pattern/patternCtrl.js
@@ -3,30 +3,32 @@ cstApp.controller('patternsConfig', ['$scope', 'resource', 'patterns', function(
     // Provide some default patterns.
     $scope.patterns = patterns.getData();
     $scope.export = { pretty: false };
-    $scope.addPattern = { regex: '', options: '', result: '' };
+    $scope.addPattern = { regex: '', modifiers: '', result: '' };
 
     $scope.$on('patternsUpdate', function(event, data) {
         $scope.patterns = patterns.getData();
         if (data.apply) $scope.$apply();
     });
-    $scope.patternAdd = function() {
+    $scope.add = function() {
         var result = patterns.addItem({
-            regex: '',
-            options: 'g',
-            result: 1
+            regex: $scope.addPattern.regex,
+            options: $scope.addPattern.modifiers,
+            result: $scope.addPattern.result
         });
         if (!result.success) {
             $('#saveConfirmPatterns').html('<div class="alert alert-danger"><a class="close" data-dismiss="alert">x</a>Failed to add pattern: '+result.message+'</div>');
+        } else {
+            $scope.addPattern = { regex: '', modifiers: '', result: '' };
         }
     };
-    $scope.patternRemove = function(index) {
+    $scope.remove = function(index) {
         patterns.removeItem(index);
     };
     $scope.export = function() {
         var patternsObject = JSON.stringify(patterns.getData(), null, ($scope.export.pretty * 4));
         $('.cst-patterns-config .cst-import-export textarea').val(patternsObject);
     }
-    $scope.savePatterns = function() {
+    $scope.save = function() {
         var result = patterns.setPatterns($scope.patterns);
         if (result.success) {
             patterns.save(function(result) {
