41ef9bd33d431e6db5d991ac1e2f288de27f9586
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2014-02-23 01:08:40 -0800

    Only loading bootstrap if it does not already exist on content context.

--- content/find-symbols.js
+++ content/find-symbols.js
@@ -53,8 +53,93 @@ var findVariables = function(html, patterns) {
     return symbolsCleaned;
 };
 
-// Only load bootstrap if it is not already present on the content page.
+/**
+ * Load a JS library; if it does not already exist in content context.
+ *
+ * All js files must be included in the manifest's web_accessible_resources.
+ *
+ * @param string file
+ *   Name of the local javascript file to load.
+ * @param array regex
+ *   (optional) An array of Regular expression objects. If present will only
+ *   load file if the content page does not have a script element with a src
+ *   attribute that matches regex pattern. If ommited then the file will
+ *   always be loaded.
+ *
+ * @return null;
+ */
+var cstLoadJS = function(file, regex) {
+    var load = function(file) {
+        $.get(chrome.extension.getURL(file), {}, function(data) {
+            eval(data);
+        });
+    }
+    var found = false;
+    if (typeof(regex) != 'undefined') {
+        for (var i in regex) {
+            $('script').each(function(index, element) {
+                var src = $(element).attr('src');
+                if (src && src.match(regex[i])) {
+                    found = true;
+                    return false;
+                }
+            });
+            if (found)
+                break;
+        }
+        if (found)
+            console.log(file + " Is already present!");
+        if (!found)
+            load(file);
+    } else {
+        load(file);
+    }
+}
+
+/**
+ * Load all JS libraries.
+ *
+ * It is neccesary to dynamically load all libraries rather than specifying
+ * them to auto load in the manifest file. Certail libraries should only be
+ * loaded conditionally, if they do not already exist in the content context.
+ * Double loading these libraries will break the content page, which is the
+ * last thing we want to do. Better to break our extension in these cases.
+ *
+ * The JS is isolated between contexs (content and tab(extension)), but the
+ * DOM is shared. Any libraries that automatically operate on the DOM will
+ * cause difficulty when loaded twice and working with a shared DOM.
+ *
+ * @return null;
+ */
+var cstLoadAllJS = function() {
+    var conditionalJs = [
+        { file: "libs/external/bootstrap/bootstrap-3.0.3/dist/js/bootstrap.min.js", regex: [ /bootstrap(?:\.min)?\.js/ ] }
+    ];
+    var alwaysJs = [
+        "libs/external/angular/angular.min.js",
+        "libs/external/bower_components/angular-ui-utils/ui-utils.min.js",
+        "libs/CSTResource.js",
+        "js/cstApp.js",
+        "js/resource/resourceFactory.js",
+        "js/links/linksFactory.js",
+        "js/links/linksCtrl.js",
+        "js/links/linksDirective.js",
+        "js/variable/variableCtrl.js",
+        "js/variable/variableDirective.js",
+        "js/variable/variableFactory.js",
+        "js/bar/barCtrl.js",
+        "js/bar/barDirective.js",
+        "js/pattern/patternCtrl.js",
+        "js/pattern/patternDirective.js"
+    ];
+    for (var i in conditionalJs)
+        cstLoadJS(conditionalJs[i].file, conditionalJs[i].regex);
+    for (var i in alwaysJs)
+        cstLoadJS(alwaysJs[i]);
+}
+
 $('document').ready(function() {
+    cstLoadAllJS();
     chrome.storage.sync.get(['patterns'], function(result) {
         if (chrome.runtime.lastError) {
             console.log('Content script can\'t read from synced storage: ' + chrome.runtime.lastError.message);
