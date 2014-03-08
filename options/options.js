f70446f1d895a0e4fe245f2eb670e5bf658593c5
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2014-03-07 23:02:28 -0800

    Only adding the last 3 posts from the money feed.

--- options/options.js
+++ options/options.js
@@ -2,11 +2,15 @@
 $(document).ready(function() {
     $.get('http://www.automatonsofmisery.com/money/feed/', function (data) {
         var output = '';
+        var count = 0;
         $(data).find("item").each(function () { // or "item" or whatever suits your feed
             var el = $(this);
             output = output + "<h4 class=\"title\">" + el.find("title").text() + "</h4>\n";
             output = output + "<div class=\"date\">" + el.find("pubDate").text() + "</div>\n";
             output = output + "<div class=\"description\">" + el.find("description").text() + "</div>\n";
+            count++;
+            if (count >= 3)
+                return false;
         });
         $('div#dashboard .news .items').html(output);
     });
